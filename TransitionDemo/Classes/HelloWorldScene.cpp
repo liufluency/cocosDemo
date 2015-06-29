8db60f5c2a1f9cb59735384743856e2f5fc3690d
liufluency <gvchina@gvchinadeMacBook-Pro.local>
2015-06-29 17:14:38 +0800

    demo实现

--- TransitionDemo/Classes/HelloWorldScene.cpp
+++ TransitionDemo/Classes/HelloWorldScene.cpp
@@ -30,47 +30,80 @@ bool HelloWorld::init()
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+    auto background = Sprite::create("bg1.jpg");
+    background->setPosition(visibleSize/2);
+    background->setScale(visibleSize.width/background->getTextureRect().getMaxX(), visibleSize.height/background->getTextureRect().getMaxY());
+    addChild(background);
     
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
+    scheduleOnce([visibleSize](float f){
+        
+        auto scene = Scene::create();
+        auto newplayground = Sprite::create("bg2.jpg");
+        newplayground->setPosition(visibleSize/2);
+        newplayground->setScale(visibleSize.width/newplayground->getTextureRect().getMaxX(), visibleSize.height/newplayground->getTextureRect().getMaxY());
+        scene->addChild(newplayground);
+        
+        //直接进行场景转换
+        //Director::getInstance()->replaceScene(scene);
+        
+        //先变暗，再出现
+        //auto transition = TransitionFade::create(1, scene);
+        
+        //淡入淡出效果
+        //auto transition = TransitionCrossFade::create(1, scene);
+        
+        //角落渐变
+        //auto transition = TransitionFadeBL::create(1, scene);    //向左下渐变
+        //auto transition = TransitionFadeDown::create(1, scene);  //向下渐变
+        //auto transition = TransitionFadeTR::create(1, scene);    //向右上渐变
+        //auto transition = TransitionFadeUp::create(1, scene);    //向上渐变
+        
+        //翻转效果
+        //auto transition = TransitionZoomFlipAngular::create(1, scene);
+        //auto transition = TransitionFlipAngular::create(1, scene);
+        //auto transition = TransitionFlipX::create(1, scene);
+        //auto transition = TransitionFlipY::create(1, scene);
+        
+        //跳动
+        //auto transition = TransitionJumpZoom::create(1, scene);
+        
+        //翻页效果。ture，false控制翻页方式
+        //auto transition = TransitionPageTurn::create(1, scene, true);
+        
+        //移入效果，上下左右
+        //auto transition = TransitionMoveInB::create(1, scene);
+        //auto transition = TransitionMoveInT::create(1, scene);
+        //auto transition = TransitionMoveInL::create(1, scene);
+        //auto transition = TransitionMoveInR::create(1, scene);
+        
+        //滑动，上下左右
+        //auto transition = TransitionSlideInB::create(1, scene);
+        //auto transition = TransitionSlideInL::create(1, scene);
+        //auto transition = TransitionSlideInT::create(1, scene);
+        //auto transition = TransitionSlideInR::create(1, scene);
+        
+        //逆时针
+        //auto transition = TransitionProgressRadialCCW::create(1, scene);
+        
+        //顺时针
+        //auto transition = TransitionProgressRadialCW::create(1, scene);
+        
+        //消失
+        //auto transition = TransitionProgressHorizontal::create(1, scene);
+        
+        //行分割
+        //auto transition = TransitionSplitRows::create(1, scene);
+        
+        //列分割
+        //auto transition = TransitionSplitCols::create(1, scene);
+        
+        //缩小放大
+        auto transition = TransitionShrinkGrow::create(1, scene);
+        
+        
+        Director::getInstance()->replaceScene(transition);
+        
+    }, 2,"text");
     
     return true;
 }
